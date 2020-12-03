40669690925a5df8dcf283ffebf9d9bd8ea29f3c
sseungjae <61439454+sseungjae@users.noreply.github.com>
2020-12-04 00:42:40 +0900

    button function
    fuonction log check

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
  const slides = document.querySelector(".slider").children;
+//console.log(slides)
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
 
- console.log(slides)
+prev.addEventListener("click", function(){
+    console.log("prev click")
+})
+
+next.addEventListener("click", function(){
+    console.log("next click")
+})
